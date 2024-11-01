4b4e2d8332d52dd5bef24fe7706e6e827fcc9c7c
valexsav <valexvasilevich@gmail.com>
2024-11-01 22:48:47 +0100

    Added templates for registration and login; updated settings, URLs, views and forms

--- propertyhunt/propertyhunt/urls.py
+++ propertyhunt/propertyhunt/urls.py
@@ -17,6 +17,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from user.views import login_view, logout_view, register_view, redirection, index_view
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    path('registration/', register_view, name='registration'),
+    path('index/', index_view, name='index'),
+    path('', redirection, name='redirection'),
 ]
--- propertyhunt/user/views.py
+++ propertyhunt/user/views.py
@@ -1,3 +1,60 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
+
+from user.forms import RegistrationForm
+
+
+def index_view(request):
+    return render(request, 'index.html') 
+
+
+def redirection(request):
+    return redirect('index')
+
+
+def login_view(request):
+    
+    if request.user.is_authenticated:
+        return redirect('index')
+
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('index')
+        
+    elif request.method == 'GET':
+        form = AuthenticationForm(request)
+
+    return render(
+        request,
+        'login.html',
+        context={'form': form}
+    )
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('logout')
+
+
+def register_view(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+
+    if request.method == 'POST':
+        form = RegistrationForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        
+    elif request.method == 'GET':
+        form = RegistrationForm()
+
+    return render(
+        request,
+        'registration.html',
+        context={'form': form}
+    )
