94ba587a36668af5a665cd1ef7be58c10ba6470f
valexsav <valexvasilevich@gmail.com>
2024-11-04 17:16:34 +0100

    added ordering and filtering for properties on main page

--- propertyhunt/property/forms.py
+++ propertyhunt/property/forms.py
@@ -7,5 +7,38 @@ class PropertyForm(forms.ModelForm):
         model = Property
         fields = ['name', 'price', 'location', 'area', 'photo']
 
+
+class PropertySortForm(forms.Form):
+    sort_by = forms.ChoiceField(
+        choices=[
+            ('', 'Выберите вариант сортировки'),
+            ('price', 'По возрастанию цены'),
+            ('-price', 'По убыванию цены'),
+            ('area', 'По возрастанию площади'),
+            ('-area', 'По убыванию площади')
+        ],
+        required=False,
+        label='Сортировать по:'
+    )
+
+
+class PropertyFilterForm(forms.Form):
+    min_price = forms.IntegerField(
+        required=False,
+        label="Минимальная цена"
+    )
     
-        
+    max_price = forms.IntegerField(
+        required=False,
+        label="Максимальная цена"
+    )
+    
+    min_area = forms.IntegerField(
+        required=False,
+        label="Минимальная площадь"
+    )
+    
+    max_area = forms.IntegerField(
+        required=False,
+        label="Максимальная площадь"
+    )
--- propertyhunt/property/views.py
+++ propertyhunt/property/views.py
@@ -3,16 +3,49 @@ from .models import Property
 from .forms import PropertyForm
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseNotAllowed
+from .forms import PropertySortForm, PropertyFilterForm
 
 
 @login_required
 def index_view(request):
+    sort_form = PropertySortForm(request.GET or None)
+    filter_form = PropertyFilterForm(request.GET or None)
     properties = Property.objects.all()
+
+    if sort_form.is_valid() and sort_form.cleaned_data:
+        sort_by = sort_form.cleaned_data.get('sort_by')
+        if sort_by:
+            properties = properties.order_by(sort_by)
+
+    if filter_form.is_valid() and filter_form.cleaned_data:
+        filters ={}
+
+        min_price = filter_form.cleaned_data.get('min_price')
+        if min_price:
+            filters['price__gte'] = min_price
+
+        max_price = filter_form.cleaned_data.get('max_price')
+        if max_price:
+            filters['price__lte'] = max_price
+
+        min_area = filter_form.cleaned_data.get('min_area')
+        if min_area:
+            filters['area__gte'] = min_area
+
+        max_area = filter_form.cleaned_data.get('max_area')
+        if max_area:
+            filters['area__lte'] = max_area
+
+        properties = properties.filter(**filters)
+    
     return render(
         request,
         'index.html',
-        context={'properties': properties}
-        ) 
+        context={
+            'properties': properties,
+            'sort_form': sort_form,
+        }
+    ) 
 
 
 def the_property_view(request, property_id):
@@ -21,7 +54,7 @@ def the_property_view(request, property_id):
         request,
         'the_property.html',
         context={'property': property}
-        )
+    )
 
 
 def my_properties_view(request):
@@ -45,7 +78,10 @@ def edit_property_view(request, property_id):
     return render(
         request,
         'edit_property.html',
-        context={'form': form, 'property': property_data}
+        context={
+            'form': form,
+            'property': property_data
+        }
     )
 
 
